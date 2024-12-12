bbbbc4920cd7f08992668efb2386cd952a322e10
mayanksagar1 <mayanksagar963@gmail.com>
2024-12-12 21:59:35 +0530

    setting up components folder

---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import {useState} from "react";
+import {AiOutlineHome, AiOutlineShopping, AiOutlineLogin, AiOutlineUserAdd, AiOutlineShoppingCart} from "react-icons/ai";
+import {FaHeart} from "react-icons/fa";
+import {Link} from "react-router";
+import {useNavigate} from "react-router-dom";
+
+const Navigation = () => {
+  return <div style={{zIndex: 9999}}></div>;
+};
+
+export default Navigation;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,9 @@
-import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import {createBrowserRouter, createRoutesFromElements, Route} from "react-router";
+import {RouterProvider} from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<App />} />));
+
+createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
