ee9299e00ce8ea83d207c6e42ee00dc246e0d711
mayanksagar1 <mayanksagar963@gmail.com>
2024-12-18 22:50:24 +0530

    made some changes to profile and account page

--- frontend/src/pages/User/Profile.jsx
+++ frontend/src/pages/User/Profile.jsx
@@ -1,6 +1,5 @@
 import {useState, useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {useNavigate, useLocation, Link} from "react-router";
 import {setCredentials} from "../../redux/features/auth/authSlice";
 import {useProfileMutation} from "../../redux/api/usersApiSlice";
 import Loader from "../../components/Loader";
@@ -15,7 +14,6 @@ const Profile = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
 
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   const {userInfo} = useSelector((state) => state.auth);
 
@@ -27,6 +25,7 @@ const Profile = () => {
   const [updateUserProfile, {isLoading: loadingUpdateProfile}] = useProfileMutation();
 
   const submitHandler = async (e) => {
+    e.preventDefault();
     if (password !== confirmPassword) {
       toast.error("Passwords do not match");
     } else {
@@ -37,15 +36,18 @@ const Profile = () => {
           email,
           password,
         }).unwrap();
+        console.log(res);
         dispatch(setCredentials({...res}));
         toast.success("Profile updated successfully");
+        setEdit(true);
       } catch (err) {
-        toast.error(err?.data?.message || err.error);
+        console.log(err);
+        toast.error(err?.data?.message || err.message);
       }
     }
   };
   return (
-    <div className="p-4 mb-5 md:w-[90%] w-[100%] shadow-md">
+    <div className="p-4 mb-5 md:w-[90%] w-[100%] border shadow-md">
       <h2 className="flex items-center p-3 justify-between font-semibold gap-3">
         Profile
         <button className="border-black border-2 flex items-center gap-2 rounded bg-violet-500 hover:bg-violet-500 p-2 text-white" onClick={() => setEdit(false)}>
@@ -53,26 +55,43 @@ const Profile = () => {
           Edit
         </button>
       </h2>
-      <form form onSubmit={submitHandler}>
+      <form onSubmit={submitHandler}>
         <div className="mb-4">
-          <label className="block  mb-2">Name</label>
-          <input disabled={edit} type="text" placeholder="Enter name" className="form-input p-4 rounded-sm w-full" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <label htmlFor="username" className="block mb-2">
+            Name
+          </label>
+          <input disabled={edit} id="username" type="text" placeholder="Enter name" className="form-input p-4 rounded-sm w-full" value={username} onChange={(e) => setUsername(e.target.value)} />
         </div>
 
         <div className="mb-4">
-          <label className="block  mb-2">Email Address</label>
-          <input disabled={edit} type="email" placeholder="Enter email" className="form-input p-4 rounded-sm w-full" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <label htmlFor="email" className="block mb-2">
+            Email Address
+          </label>
+          <input disabled={edit} id="email" type="email" placeholder="Enter email" className="form-input p-4 rounded-sm w-full" value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
 
         <div className="mb-4">
-          <label className="block  mb-2">Password</label>
-          <input disabled={edit} type="password" placeholder="Enter password" className="form-input p-4 rounded-sm w-full" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-
-        <div className="mb-4">
-          <label className="block mb-2">Confirm Password</label>
+          <label htmlFor="password" className="block  mb-2">
+            Password
+          </label>
           <input
             disabled={edit}
+            id="password"
+            type="password"
+            placeholder="Enter password"
+            className="form-input p-4 rounded-sm w-full"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="confirmPassword" className="block mb-2">
+            Confirm Password
+          </label>
+          <input
+            disabled={edit}
+            id="confirmPassword"
             type="password"
             placeholder="Confirm password"
             className="form-input p-4 rounded-sm w-full"
@@ -86,6 +105,7 @@ const Profile = () => {
             Update
           </button>
         </div>
+        {loadingUpdateProfile && <Loader />}
       </form>
     </div>
   );
