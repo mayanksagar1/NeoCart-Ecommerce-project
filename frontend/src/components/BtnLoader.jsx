dc57b901bfdf5b9304944db9ed2fa1d4cba59237
Mayank Sagar <146001617+mayanksagar1@users.noreply.github.com>
2025-01-18 21:32:33 +0530

    Updated BtnLoader.jsx

--- frontend/src/components/BtnLoader.jsx
+++ frontend/src/components/BtnLoader.jsx
@@ -1,7 +1,7 @@
 const BtnLoader = () => {
   return (
     <div className="w-fit m-auto">
-      <div className="animate-spin rounded-full w-8 h-8 border-t-4 border-black"></div>;
+      <div className="animate-spin rounded-full w-8 h-8 border-t-4 border-black"></div>
     </div>
   );
 };
