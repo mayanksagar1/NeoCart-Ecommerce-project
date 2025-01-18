3532520e8df94a5eb56d61209959895477cc6605
mayanksagar1 <mayanksagar963@gmail.com>
2025-01-18 20:51:33 +0530

    updated BtnLoader.jsx

--- frontend/src/components/BtnLoader.jsx
+++ frontend/src/components/BtnLoader.jsx
@@ -1,5 +1,9 @@
 const BtnLoader = () => {
-  return <div className="animate-spin rounded-full w-8 h-8 border-t-4 border-black"></div>;
+  return (
+    <div className="w-fit m-auto">
+      <div className="animate-spin rounded-full w-8 h-8 border-t-4 border-black"></div>;
+    </div>
+  );
 };
 
 export default BtnLoader;
